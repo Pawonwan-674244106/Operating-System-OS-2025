d4d4ed226fd01581df48681e8228984793b611be
Pawonwan-674244106 <674244106@mail.pbru.ac.th>
2025-09-18 11:19:14 +0700

    Add files via upload

---
+++ lab 1/chil1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main() {
+	printf("My bio.: \n");
+	printf("ID : 674244106 \n");
+	printf("Name : Pawonwan Sawaddach\n");
+	printf("------------------------------\n");
+	getchar();
+}
