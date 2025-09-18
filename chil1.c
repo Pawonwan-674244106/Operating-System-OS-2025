642e1d24cb5136150f6465402adc1979b87019f0
Pawonwan-674244106 <674244106@mail.pbru.ac.th>
2025-09-18 11:20:20 +0700

    Delete chil1.c

--- chil1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-main() {
-	printf("My bio.: \n");
-	printf("ID : 654244001 \n");
-	printf("Name : Suwat Tachaphetpiboon\n");
-	printf("------------------------------\n");
-	getchar();
-}
