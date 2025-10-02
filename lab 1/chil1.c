bc13334d80b930ce16e6ff8db9026b885b6ca094
Pawonwan-674244106 <674244106@mail.pbru.ac.th>
2025-10-02 11:39:33 +0700

    Delete lab 1 directory

--- lab 1/chil1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-main() {
-	printf("My bio.: \n");
-	printf("ID : 674244106 \n");
-	printf("Name : Pawonwan Sawaddach\n");
-	printf("------------------------------\n");
-	getchar();
-}
